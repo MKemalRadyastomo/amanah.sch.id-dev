2ea04948f40c64b7fafc33c8e0c1de19b26844d3
M Kemal R <mkradyastomo@gmail.com>
2025-08-04 16:16:06 +0700

    [kemal] update role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,13 +5,18 @@ namespace Database\Seeders;
 use App\Enums\Roles;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
         foreach (Roles::cases() as $role) {
-            Role::findOrCreate($role->value);
+            // Gunakan firstOrCreate untuk mencegah error duplikasi
+            Role::firstOrCreate(['name' => $role->value]);
         }
     }
 }
